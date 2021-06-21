b8134afb6f27fac7b0235c6b400b4de0df781908
Luis Rodrigues <luisitaloar@gmail.com>
2021-06-21 09:22:21 -0300

    feat: added generic cards on signInp page

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
 
 import GenericCard from '../Components/GenericCard';
-// import { Container } from './styles';
+import { Button, CardDeck } from 'reactstrap';
 
 const SignIn: React.FC = () => {
   return (
     <div>
-      <GenericCard
-        title={'Login'}
-        description={{
-          value: 'Something description like really very big',
-          type: 'textarea',
-        }}
-        img={'https://borlabs.io/wp-content/uploads/2019/09/blog-wp-login.png'}
-        subtitle={'username'}
-      />
+      <CardDeck className={'App'}>
+        <GenericCard
+          title={'Login'}
+          secondInput={{ value: '', type: 'password' }}
+          firstInput={'username'}
+          button={{
+            text: 'Sign In',
+            onSubmit: (subtitle, description) =>
+              console.log('logging in with', subtitle, description),
+          }}
+          img={
+            'https://borlabs.io/wp-content/uploads/2019/09/blog-wp-login.png'
+          }
+        >
+          <input type={'checkbox'} />
+        </GenericCard>
+
+        <GenericCard
+          title={'Github Repo'}
+          firstInput={'Test Repo subtitle'}
+          button={{
+            text: 'View more',
+            onSubmit: (subtitle, description) =>
+              console.log('must zoom in', subtitle, description),
+          }}
+          secondInput={{
+            value:
+              'This github repo is awesome for testing cause his description is fucking big, big like your mother!! Gotcha !!',
+            type: 'textarea',
+          }}
+          img={
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Github-desktop-logo-symbol.svg/1024px-Github-desktop-logo-symbol.svg.png'
+          }
+        >
+          <Button placeholder={'Favorites'} />
+        </GenericCard>
+      </CardDeck>
     </div>
   );
 };
