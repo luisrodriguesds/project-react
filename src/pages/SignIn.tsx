91223363d346f82e61234fc1681c179e49107011
Luis Rodrigues <luisitaloar@gmail.com>
2021-06-22 11:17:37 -0300

    feat: Implemented the bootstrap alert

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import GenericCard from "../Components/GenericCard";
 import { Container, Row, Col } from "reactstrap";
 import { useAuth } from "../contexts/auth";
-
+import Alert from "../Components/AlertMessage";
 const SignIn: React.FC = () => {
   const { signIn } = useAuth();
+  const [messageError, setMessageError] = useState("");
+  const [errorIsOpen, setErrorIsOpen] = useState(false);
 
   const handleSubmit = async (email: string, password: string) => {
     console.log(email, password, "aqui");
     if ([email, password].some((item) => item === "")) {
-      alert("Um ou mais campo estão vazios!");
+      setMessageError("Um ou mais campo então vazios");
+      setErrorIsOpen(true);
+      return;
     }
 
     // call the conext api
@@ -25,9 +29,15 @@ const SignIn: React.FC = () => {
   };
 
   return (
-    <Container className="vh-100">
+    <Container className="vh-100 d-flex align-items-center">
       <Row className="mx-auto mt-4">
         <Col sm="12" md={{ size: 6, offset: 3 }}>
+          <Alert
+            message={messageError}
+            isOpen={errorIsOpen}
+            handleIsOpen={setErrorIsOpen}
+            variant="danger"
+          />
           <GenericCard
             title={"Please Sign In"}
             secondInput={{ value: "", type: "password" }}
