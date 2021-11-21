7b7eac2218f46180f268c6bc65033d290c3ead25
Luis Rodrigues <luis.rodrigues7@vodafone.com>
2021-11-21 20:18:45 +0000

    feat: validation with yup

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -6,72 +6,30 @@ import { useAuth } from "../contexts/auth";
 import Alert from "../components/AlertMessage";
 import Button from "../components/Button";
 import { Card, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const SignIn: React.FC = () => {
   // const { signIn } = useAuth();
 
-  // const [form, setForm] = useState<IInputCard[]>([
-  //   {
-  //     name: "email",
-  //     value: "",
-  //     placeholder: "Enter email",
-  //     label: "Email Address",
-  //     type: "email",
-  //   },
-  //   {
-  //     name: "password",
-  //     value: "",
-  //     placeholder: "Password",
-  //     label: "Password",
-  //     type: "password",
-  //   },
-  // ]);
-
-  const [btnLoading, setBtnLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    mode: "all",
+  });
 
   const [messageError, setMessageError] = useState("");
   const [errorIsOpen, setErrorIsOpen] = useState(false);
 
-  // function handleForm(value: IInputCard[]) {
-  //   setForm(value);
-  // }
-
-  // async function onSubmitForm(formValues: IInputCard[]) {
-  //   setBtnLoading(true);
-
-  //   await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  //   const [email, password] = Object.values(
-  //     formValues.map((item) => item.value)
-  //   );
-
-  //   try {
-  //     if (!email) {
-  //       throw new Error("Email is empty...");
-  //     }
-
-  //     if (!/\S+@\S+\.\S+/.test(email)) {
-  //       throw new Error("Email is not valid...");
-  //     }
-
-  //     if (!password) {
-  //       throw new Error("Password is empty...");
-  //     }
-
-  //     // call the conext api
-  //     await signIn({
-  //       email: email || "",
-  //       password: password || "",
-  //     });
-  //   } catch (error: any) {
-  //     setMessageError(error.message);
-  //     setErrorIsOpen(true);
-  //     setBtnLoading(false);
-  //   }
-  // }
+  async function onSubmitForm(data: any) {
+    console.log(data);
+  }
 
   return (
     <Container className="vh-100 d-flex align-items-center">
+      {console.log(errors)}
       <Row className="mx-auto">
         <Col sm="12" lg="12">
           <Alert
@@ -83,24 +41,42 @@ const SignIn: React.FC = () => {
           <Card bg="light" text="dark" style={{ minWidth: "380px" }}>
             <Card.Body className="p-4">
               <Card.Title className="text-center mb-4">Login Page</Card.Title>
-              <Form>
+              <Form onSubmit={handleSubmit(onSubmitForm)}>
                 <Form.Group className="mb-3">
                   <Form.Label>Email Address</Form.Label>
                   <Form.Control
                     type="text"
-                    name="email"
                     placeholder="Entrer Email"
-                    onChange={() => {}}
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "Campo vazio",
+                      },
+                    })}
                   />
+                  {errors.email && (
+                    <Form.Text style={{ color: "red" }}>
+                      {errors.email.message}
+                    </Form.Text>
+                  )}
                 </Form.Group>
                 <Form.Group className="mb-3">
                   <Form.Label>Password</Form.Label>
                   <Form.Control
-                    type="text"
-                    name="password"
+                    type="password"
                     placeholder="Entrer Password"
-                    onChange={() => {}}
+                    {...register("password", {
+                      required: {
+                        value: true,
+                        message: "Campo vazio",
+                      },
+                    })}
                   />
+                  {errors.password && (
+                    <Form.Text style={{ color: "red" }}>
+                      {errors.password.message}
+                    </Form.Text>
+                  )}
                 </Form.Group>
                 <Button
                   variant="dark"
@@ -116,23 +92,6 @@ const SignIn: React.FC = () => {
               © Celfocus, June 2021
             </Card.Footer>
           </Card>
-
-          {/* <Card
-            id={"signinCard"}
-            varient="light"
-            className="p-4"
-            header="Login Panel"
-            style={{ minWidth: "380px" }}
-            image="/assets/logo-login.png"
-            footer={`© Celfocus, June 2021`}
-            inputs={form}
-            handleInput={handleForm}
-            onSubmitForm={onSubmitForm}
-            buttonsubmit={{
-              label: "Sign In",
-              isLoading: btnLoading,
-            }}
-          ></Card> */}
         </Col>
       </Row>
     </Container>
